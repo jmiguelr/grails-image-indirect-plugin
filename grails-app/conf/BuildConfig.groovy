671f812eadd7db87669ecf511a0935dc49875fe1
jmiguel rodriguez <jmiguel@virtualsw.com>
2013-08-06 16:45:01 +0200

    Removed some unneeded files. Version 0.1.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -15,13 +15,6 @@ grails.project.dependency.resolution = {
         }
     }
 
-//    grails.project.repos.default="nexusvirtualsw"
-//    grails.project.repos.nexusvirtualsw.url = "http://nexus.virtualsw.es/nexus/content/repositories/grails_virtualsw/"
-//    grails.project.repos.nexusvirtualsw.type = "maven"
-//    grails.project.repos.nexusvirtualsw.username = "admin"
-//    grails.project.repos.nexusvirtualsw.password = "qwe123546"
-
-    // grails.project.repos.nexusvirtualsw.portal = "grailsCentral"
 }
 
 grails.project.repos.default = "nexusvirtualswgrails"
